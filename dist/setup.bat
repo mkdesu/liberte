1d8ad7a0d2b51272e8bda9b0333f58338c6767b3
Maxim Kammerer <mk@dee.su>
2010-09-14 23:02:41 +0000

    Some SYSLINUX-related improvements.

--- dist/setup.bat
+++ dist/setup.bat
@@ -2,14 +2,20 @@
 
 IF NOT EXIST %~d0\liberte\liberte.tag GOTO baddir
 
+ECHO SYSLINUX setup requires administrator privileges for raw disk access.
+ECHO If you see a message about MBR update failure below, please
+ECHO right-click on the script and select "Run as administrator".
+ECHO.
+
 ECHO Installing SYSLINUX on %~d0, optionally with bootloader.
+ECHO.
 %~d0\liberte\boot\syslinux\syslinux.exe -m -a -d /liberte/boot/syslinux %~d0
 
-IF ERRORLEVEL 0 ECHO Success!
 GOTO end
 
 :baddir
-ECHO The directory "%~pd0" should be located at the root of a removable disk.
+ECHO The directory "%~pd0" should be moved to the root of a removable disk.
 
 :end
+ECHO.
 PAUSE
