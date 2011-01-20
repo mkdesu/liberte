140296f33e94cae694fdc1b9fcc9224adecd6d7d
Maxim Kammerer <mk@dee.su>
2011-01-20 04:42:47 +0000

    Fixed shutdown on no-loop QEMU boots (1/2).

--- src/etc/init.d/reboot.sh
+++ src/etc/init.d/reboot.sh
@@ -1,9 +1,8 @@
 # Sourced at the end of halt.sh
-# Add -f so that reboot works for runlevel 0, too
 opts="-fdk"
 [ "${RC_DOWN_INTERFACE}" = "yes" ] && opts="${opts}i"
 
-/sbin/reboot "${opts}" 2>/dev/null
+/sbin/reboot "${opts}"
 
-echo KEXEC-based reboot/shutdown failed
-/sbin/poweroff -nf
+echo Normal reboot failed
+/sbin/reboot -fn
