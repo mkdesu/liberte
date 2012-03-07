ff36ac2a589053eacadcf2aef0b648e0b373d60c
Maxim Kammerer <mk@dee.su>
2012-03-07 04:53:03 +0300

    Miscellaneous boot process improvements
    The root account is now disabled by default, and enabled on-demand
    by gentoo=root.
    
    The cdroot_type=auto boot parameter is now supported by preloading
    all supported filesystem modules in initramfs (as Busybox's mount
    does not support module autoloading).
    
    Added extensive kernel parameters documentation in syslinux.cfg.
    
    Trimmed probed block device patterns in initramfs. User can still
    specify devices / glob patterns using cdroot=... boot parameter.
    
    Minor additions to kernel hardware support.

--- src/root/initrd/modules.fs
+++ src/root/initrd/modules.fs
@@ -1,6 +1,9 @@
 # Modules to force-load during initramfs stage
+# (no module autoloading on mount in Busybox)
 force_load_vfat='vfat nls_cp437 nls_iso8859-1 nls_utf8'
 force_load_iso9660='isofs nls_iso8859-1 nls_utf8'
 force_load_ext2='ext4'
 force_load_ext3='ext4'
 force_load_ext4='ext4'
+
+force_load_auto="${force_load_vfat} ${force_load_iso9660} ${force_load_ext4}"
